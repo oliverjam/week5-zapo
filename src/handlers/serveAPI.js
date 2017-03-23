fc56967d959482e88efc90434dc59f4805e7c5a7
Zooey <zooey@lbk.org.uk>
2017-03-23 17:56:28 +0000

    modularise serveAPI.js our serveAPI function was getting pretty big so we modularised it into a separate js file relates to #2

---
+++ src/handlers/serveAPI.js
@@ -0,0 +1,25 @@
+const getObits = require('../getObits.js');
+const createDates = require('../createDates.js');
+
+function serveAPI(request, response, date) {
+  let datesArr;
+  if (date) {
+    datesArr = createDates.createDateArr(date);
+  } else {
+    datesArr = createDates.createDateArr(createDates.createDate(createDates.myDate));
+  }
+  getObits(datesArr, (err, res) => {
+    if (err) {
+      // if error send 500 error to browser
+      // return err;
+      response.writeHead(500, { 'Content-Type': 'text/html', 'access-control-allow-origin': '*' });
+      response.end("<h1>Sorry, we've got some problems on our side, don't worry, it'll all be over soon</h1>");
+    }
+    res.sort((a, b) => b.date.slice(0, 4) - a.date.slice(0, 4));
+    // console.log(res);
+    response.writeHead(200, { 'Content-Type': 'application/json', 'access-control-allow-origin': '*' });
+    response.end(JSON.stringify(res));
+  });
+}
+
+module.exports = serveAPI;
